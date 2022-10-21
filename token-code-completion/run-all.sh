4b5411f8cf71851ac22b71e00b8b78c88e889c0b
Jurriaan Den Toonder <1493561+Fastjur@users.noreply.github.com>
2022-10-21 17:39:23 +0200

    Add run-all script, make max training steps 10

---
+++ token-code-completion/run-all.sh
@@ -0,0 +1,7 @@
+(make py-train-token-on-js || echo "Failed to train py token on JS"); \
+(rm save/py150/train_blocksize_1024_wordsize_1_rank_0); \
+(make py-train-token-on-ts || echo "Failed to train py token on TS"); \
+(rm save/py150/train_blocksize_1024_wordsize_1_rank_0); \
+(make ts-train-token || echo "Failed to train java token on TS"); \
+(make js-train-token || echo "Failed to train java token on JS"); \
+echo "Done!"
