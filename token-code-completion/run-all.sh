5fa92c40e3ce85b7bcc49a422ba47c166194784a
RubenNair <44524104+RubenNair@users.noreply.github.com>
2022-10-28 16:52:24 +0200

    add py finetune on py command to run-all.sh
    This way the bash script can generate all our single-language models

--- token-code-completion/run-all.sh
+++ token-code-completion/run-all.sh
@@ -1,3 +1,4 @@
+(make py-train-token || echo "Failed to train py token on py"); \
 (make py-train-token-on-js || echo "Failed to train py token on JS"); \
 (rm save/py150/train_blocksize_1024_wordsize_1_rank_0); \
 (make py-train-token-on-ts || echo "Failed to train py token on TS"); \
